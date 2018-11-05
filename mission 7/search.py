070042ad6df774bf8c60f872f364fa95ba9889a6
wiauxb <bastien.wiaux@gmail.com>
2018-11-05 17:53:10 +0100

    added get_words

--- mission 7/search.py
+++ mission 7/search.py
@@ -3,10 +3,40 @@ def readfile(filename):
             "this alarming chain of events, the Supreme Chancellor has",\
             "secretly dispatched two Jedi Knights."]
 
+
+def positions(text,car):
+    """
+    pre: prend un 2 strings text, car
+    post:retourne une liste avec le(s) index(s) de "car"
+    """
+    text,car = text.lower(),car.lower()
+    index = []
+    etape = 0
+    while car in text:
+        index.append(text.index(car)+etape)
+        etape += text.index(car)+len(car)
+        text = text[text.index(car)+len(car):]
+    return index
+
+def clean(mot,p):
+    for i in positions(mot,p):
+        mot = mot[:i]+mot[i+1:]
+    return mot
+
 def get_words(line):
-    return ["turmoil", "has", "engulfed", "the", "galactic", "republic"]
+    lst = []
+    for mot in line.strip().lower().split(" "):
+        for ponct in [".",",",":","?","!","/",";"]:
+            if ponct in mot:
+                mot = clean(mot,ponct)
+        lst.append(mot.lower())
+    return lst
 
 def create_index(filename):
+    """
+    pre: prend un chemin filename verss un fichier texte
+    post: retourne un index pour le fichier avec nom filename
+    """
     words = [get_words(l) for l in readfile(filename)]
     dct = {}
     for index,ligne in enumerate(words):
