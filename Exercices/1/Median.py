40486ac8fd7b3eb69dfbc76d1499d5b23e18b52f
wiauxb <bastien.wiaux@gmail.com>
2019-01-11 22:02:44 +0100

    setup the exercices

---
+++ Exercices/1/Median.py
@@ -0,0 +1,6 @@
+if (a>=b and a<=c) or (a>=c and a<=b):
+    median = a
+elif (b>=a and b<=c) or (b>=c and b<=a):
+    median = b
+else:
+    median = c
---
+++ Exercices/1/QBF.py
@@ -0,0 +1,7 @@
+print(s0)
+while s0 != 1:
+    if s0%2 == 0:
+        s0 =int(s0/2)
+    else:
+        s0 = 3*s0+1
+    print(s0)
