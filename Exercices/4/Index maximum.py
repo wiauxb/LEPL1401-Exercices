5bce31db449fceb146ae5b1dc2be47d603b2e4b7
Robin Verschuren <robin.ver@hotmail.com>
2019-01-12 14:29:59 +0100

    Update Index maximum.py

--- Exercices/4/Index maximum.py
+++ Exercices/4/Index maximum.py
@@ -1,5 +1,6 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 def maximum(lst):
     max = int()
     for i in lst:
@@ -9,3 +10,14 @@ def maximum(lst):
 
 def maximum_index(lst):
     return None if len(lst) == 0 else lst.index(maximum(lst))
+
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+def maximum_index(lst):
+    if len(lst) == 0 :
+        return None
+    else :
+        max_value = sorted(lst)[-1]
+        return lst.index(max_value)
