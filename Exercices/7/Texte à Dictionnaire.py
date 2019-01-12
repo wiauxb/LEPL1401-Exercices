557bcd8b66832559e93b9e43e6ef868568b98da0
Robin Verschuren <robin.ver@hotmail.com>
2019-01-12 15:18:34 +0100

    Update Texte à Dictionnaire.py

--- Exercices/7/Texte à Dictionnaire.py
+++ Exercices/7/Texte à Dictionnaire.py
@@ -1,5 +1,6 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 def create_dictionary(filename):
     lst = []
     with open(filename,'r') as f:
@@ -16,3 +17,17 @@ def create_dictionary(filename):
 
 def occ(d, word):
     return d.get(word,0)
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+def create_dictionary(filename):
+    d = {}
+    with open(filename, 'r') as file :
+        for line in file.readlines():
+            for word in line.split():
+                d[word] = d.get(word, 0) + 1
+    return d
+
+def occ(dictionary, word):
+    return dictionary.get(word, 0)
