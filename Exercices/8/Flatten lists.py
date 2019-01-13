010ba7edeccfcf69d8ee35128fdb16cdc44e0e08
Robin Verschuren <robin.ver@hotmail.com>
2019-01-13 21:44:01 +0100

    Update Flatten lists.py

--- Exercices/8/Flatten lists.py
+++ Exercices/8/Flatten lists.py
@@ -1,8 +1,21 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 def flatten(l):
     if l == []:
         return []
     elif type(l[0]) == list:
         return flatten(l[0]) + flatten(l[1:])
     return [l[0]] + flatten(l[1:])
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+def flatten(l):
+    lst = []
+    for item in l:
+        if isinstance(item, list):
+            lst += flatten(item)
+        else:
+            lst.append(item)
+    return lst
