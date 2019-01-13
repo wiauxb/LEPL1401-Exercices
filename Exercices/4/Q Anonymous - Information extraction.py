5846ddb8aaa0bfe391c55537f6d25b8c271602d7
Robin Verschuren <robin.ver@hotmail.com>
2019-01-13 13:04:29 +0100

    Update Q Anonymous - Information extraction.py

--- Exercices/4/Q Anonymous - Information extraction.py
+++ Exercices/4/Q Anonymous - Information extraction.py
@@ -1,5 +1,6 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 def extract(code):
     rep = []
     for i in code:
@@ -15,3 +16,25 @@ def extract(code):
             else:
                 rep[-1]+="-up"
     return " ".join(rep)
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+def extract(code):
+    
+    def upOrLow(char):
+        if char.lower() == char:
+            return '-low '
+        else :
+            return '-up '
+        
+    rt_string = ''
+    for char in code:
+        if char.lower() in 'aeiouy':
+            rt_string += 'vowel' + upOrLow(char)
+        elif char in '0123456789':
+            rt_string += 'number '
+        else:
+            rt_string += 'consonant' + upOrLow(char) 
+    return rt_string
+            
