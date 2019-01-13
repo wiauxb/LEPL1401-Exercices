c0e8a524f17d46bf9d7dce826a332684c9a7ee0d
Robin Verschuren <robin.ver@hotmail.com>
2019-01-13 14:09:42 +0100

    Update Compteur d'événement.py

--- Exercices/5/Compteur d'événement.py
+++ Exercices/5/Compteur d'événement.py
@@ -1,8 +1,19 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 def count(events, i, j):
     count = 0
     for a,b in events:
         if a == i and b == j:
             count += 1
     return count
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+def count(events,i,j):
+    n = 0
+    for coord in events:
+        if coord == (i,j):
+            n += 1
+    return n
