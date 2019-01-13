71b33c81b4f2fe216e0f2a275643ac1b42852e2d
Robin Verschuren <robin.ver@hotmail.com>
2019-01-13 14:17:38 +0100

    Update Compteur d'événements (Matrice).py

--- Exercices/5/Compteur d'événements (Matrice).py
+++ Exercices/5/Compteur d'événements (Matrice).py
@@ -1,5 +1,6 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 def counts(events, n, m):
     matrix = []
     for i in range(n):
@@ -11,4 +12,16 @@ def counts(events, n, m):
                     count += 1
             ligne.append(count)
         matrix.append(ligne)
+    return matrix
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+def counts(events, n, m):
+    matrix = [ [0 for i in range(n)] for j in range(m)]
+    
+    for event in events:
+        i = event[0]
+        j = event[1]
+        matrix[i][j] += 1
     return matrix
