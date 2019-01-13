d9ec129a9ad123ec3ddf15d6ec01205854f3efc2
Robin Verschuren <robin.ver@hotmail.com>
2019-01-13 15:32:51 +0100

    Update Q Tri de liste.py

--- Exercices/5/Q Tri de liste.py
+++ Exercices/5/Q Tri de liste.py
@@ -1,5 +1,6 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 sorted_list = ["_" for i in range(len(a_list))]
 for i in a_list:
     for j,k in enumerate(sorted_list):
@@ -10,4 +11,17 @@ for i in a_list:
             for l in range(len(sorted_list)-1,j,-1):
                 sorted_list[l] = sorted_list[l-1]
             sorted_list[j] = i
+            break
+            
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+sorted_list = [a_list[0]]
+for item in a_list[1:] :
+    for index, value in enumerate(sorted_list):
+        if item < value:
+            sorted_list.insert(index, item)
+            break
+        elif len(sorted_list) == index + 1 :
+            sorted_list.append(item)
             break
