19a84129f10b96a9f255c001fca357eb859ee0ba
Robin Verschuren <robin.ver@hotmail.com>
2019-01-14 13:43:16 +0100

    Resolving format errors
    Pour utiliser une balise 'begin { ___ }' il faut faire attention avec la fonction format de python. Pour que {} soit utilisé comme chaine et non pas comme accesseur, il faut transformer  'begin { ___ }' en 'begin { {___ } }'

--- Exercices/ingi2tex/code/fnc_tex.py
+++ Exercices/ingi2tex/code/fnc_tex.py
@@ -4,25 +4,24 @@ import file_reader as fr
 
 def insertAll(args) :
     d = settings.getAllContent()
-    print(d)
     t = generateTexSession(d)
-    print(t)
     return t
 
 def generateTexSession(sessions):
     t = ""
     for sessionName, exercices in sessions.items():
-        t += " {0}".format(sessionName)
+        t += "\\section{{ {0} }}\n".format(sessionName)
         t += generateTexExercices(exercices[1:])
     return t
 
 def generateTexSolutions(solutions):
     t = ""
     for solution in solutions:
-        t += """solution proposée par {0[author]}
-        \\ begin { lstlisting }[language=python]
-        {0[code]}
-        \\ end { lstlisting }\n""".format(solutions)   
+        t += """
+solution proposée par {0[author]}
+\\begin {{ lstlisting }}[language=python]
+{0[code]}
+\\end {{ lstlisting }}\n""".format(solution)
 
     return t
 
@@ -33,11 +32,11 @@ def generateTexExercices(exercices):
     for exerc in exercices:
         print("EXERCICE", exerc)
         f = fr.FileReader(exerc['location'])
-        
-        t += """\\subsection {0[name]}
-        Disponible via ce lien {0[url]}\n""".format(exerc)
-        t+= generateTexSolutions(f)+"\n"
-        
+
+        t += """
+\\subsection {0[name]}\n
+Disponible via ce lien {0[url]}\n""".format(exerc)
+        t+= generateTexSolutions(f.getStructured())+"\n"
+
         print(f.getStructured())
     return t
-
